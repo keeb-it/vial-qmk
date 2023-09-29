b2aa804b35a2570e9b535c9358cc5212dd94eb24
numToStr <vikasraj1911@gmail.com>
2023-09-29 21:32:57 +0530

    init fw

---
+++ keyboards/keebit/coral/keymaps/vial/config.h
@@ -0,0 +1,10 @@
+// Copyright 2023 numToStr (@numToStr)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+// Vial
+#define VIAL_KEYBOARD_UID {0x5E, 0x76, 0x43, 0x48, 0xD6, 0xE1, 0x5C, 0x95}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 1 }
+#define DYNAMIC_KEYMAP_LAYER_COUNT 8
